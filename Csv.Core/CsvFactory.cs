4d1f0fed1912cd84fe81d229259c4423348c04c2
Andy <andyjhorn@gmail.com>
2021-08-27 14:32:01 -0500

    added a CsvFactory

---
+++ Csv.Core/CsvFactory.cs
@@ -0,0 +1,15 @@
+﻿using Csv.Core.Interfaces;
+
+namespace Csv.Core
+{
+    /// <summary>
+    /// Factory class for creating new instances of the <see cref="ICsv"/> interface.
+    /// </summary>
+    public static class CsvFactory
+    {
+        /// <summary>
+        /// Gets a new instance of the <see cref="ICsv"/> interface.
+        /// </summary>
+        public static ICsv New { get => new Models.Csv(); }
+    }
+}
---
+++ Csv.Test/CsvFactoryTests.cs
@@ -0,0 +1,26 @@
+﻿using Csv.Core;
+using NUnit.Framework;
+
+namespace Csv.Test
+{
+    [TestFixture]
+    public class CsvFactoryTests
+    {
+        [Test]
+        public void ReturnsNewInstance()
+        {
+            var csv = CsvFactory.New;
+
+            Assert.IsNotNull(csv);
+        }
+
+        [Test]
+        public void ReturnsNewInstances()
+        {
+            var csvOne = CsvFactory.New;
+            var csvTwo = CsvFactory.New;
+
+            Assert.AreNotSame(csvOne, csvTwo);
+        }
+    }
+}
