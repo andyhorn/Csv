bb8b9fa234393a86f7964ca04d300b011caf45f7
Andy <andyjhorn@gmail.com>
2021-08-30 16:23:39 -0500

    added templated interface; child of main interface

---
+++ Csv.Core/Interfaces/ICsvTemplated.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+namespace Csv.Core.Interfaces
+{
+    public interface ICsv<T> : ICsv
+    {
+        void Add(T item);
+
+        void AddRange(ICollection<T> items);
+
+        T Get(int index);
+
+        ICollection<T> Get();
+
+        void Remove(int index);
+
+        Dictionary<PropertyInfo, string> HeaderMap { get; set; }
+
+        List<PropertyInfo> Ignores { get; set; }
+    }
+}
