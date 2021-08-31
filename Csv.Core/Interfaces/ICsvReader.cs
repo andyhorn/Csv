59ba445930847a7e9758ed6ce5b56c4c73eff372
Andy <andyjhorn@gmail.com>
2021-08-30 21:43:03 -0500

    converted reader to concrete class with interface; implemented a templated child class

---
+++ Csv.Core/Interfaces/ICsvReader.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+
+namespace Csv.Core.Interfaces
+{
+    /// <summary>
+    /// Defines an interface for reading CSV-formatted
+    /// data and producing an <see cref="ICsv"/> instance.
+    /// </summary>
+    public interface ICsvReader
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether the
+        /// CSV-formatted data contains a row of headers.
+        /// </summary>
+        bool HasHeaders { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value that separates individual
+        /// cells in the CSV-formatted data.
+        /// </summary>
+        char Separator { get; set; }
+
+        /// <summary>
+        /// Parses a string of CSV-formatted data into a new
+        /// <see cref="ICsv"/> instance.
+        /// </summary>
+        /// <param name="csvData">A string containing
+        /// CSV-formatted data.</param>
+        /// <returns>A new <see cref="ICsv"/> instance.</returns>
+        ICsv FromString(string csvData);
+
+        /// <summary>
+        /// Parses a collection of strings containing CSV-formatted
+        /// data into a new <see cref="ICsv"/> instance.
+        /// </summary>
+        /// <param name="lines">An array of strings containing
+        /// CSV-formatted data.</param>
+        /// <returns>A new <see cref="ICsv"/> instance.</returns>
+        ICsv FromLines(string[] lines);
+
+        /// <summary>
+        /// Reads and parses a CSV-formatted file to produce a
+        /// new <see cref="ICsv"/> instance.
+        /// </summary>
+        /// <param name="fileName">The filename for a CSV file.</param>
+        /// <returns>A new <see cref="ICsv"/> instance.</returns>
+        ICsv FromFile(string fileName);
+
+        /// <summary>
+        /// Reads CSV-formatted data from a <see cref="Stream"/> source
+        /// and produces a new <see cref="ICsv"/> instance.
+        /// </summary>
+        /// <param name="stream">A <see cref="Stream"/> object with
+        /// CSV-formatted data to be read.</param>
+        /// <returns>A new <see cref="ICsv"/> instance.</returns>
+        ICsv FromStream(Stream stream);
+    }
+}
---
+++ Csv.Core/Readers/CsvReader.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Csv.Core.Interfaces;
+
+namespace Csv.Core.Readers
+{
+    /// <summary>
+    /// Implements the <see cref="ICsvReader"/> interface.
+    /// </summary>
+    public class CsvReader : ICsvReader
+    {
+        private int _numColumns = 0;
+        protected string[] _newLineOperators
+            = new string[] { "\r", "\n", "\r\n" };
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CsvReader"/> class.
+        /// </summary>
+        public CsvReader()
+        {
+            HasHeaders = true;
+            Separator = ',';
+        }
+
+        /// <inheritdoc/>
+        public bool HasHeaders { get; set; }
+
+        /// <inheritdoc/>
+        public char Separator { get; set; }
+
+        /// <inheritdoc/>
+        public virtual ICsv FromFile(string fileName)
+        {
+            if (string.IsNullOrWhiteSpace(fileName))
+            {
+                throw new ArgumentNullException(nameof(fileName));
+            }
+
+            if (!File.Exists(fileName))
+            {
+                throw new FileNotFoundException();
+            }
+
+            using var stream = File.OpenRead(fileName);
+
+            var csv = FromStream(stream);
+
+            (csv as Models.Csv).Filename = fileName;
+
+            return csv;
+        }
+
+        /// <inheritdoc/>
+        public ICsv FromLines(string[] lines)
+        {
+            var csv = GetCsv();
+
+            SetHeaders(csv, lines);
+            AddAllRows(csv, lines);
+
+            return csv;
+        }
+
+        /// <inheritdoc/>
+        public ICsv FromStream(Stream stream)
+        {
+            if (stream == null || !stream.CanRead)
+            {
+                throw new ArgumentException("Cannot read source.");
+            }
+
+            var csv = GetCsv();
+
+            using var reader = new StreamReader(stream);
+
+            SetHeaders(csv, reader);
+
+            var currentLine = reader.ReadLine();
+            while (!string.IsNullOrWhiteSpace(currentLine))
+            {
+                AddRow(csv, currentLine);
+
+                currentLine = reader.ReadLine();
+            }
+
+            return csv;
+        }
+
+        /// <inheritdoc/>
+        public ICsv FromString(string csvData)
+        {
+            var lines = csvData.Split(_newLineOperators, StringSplitOptions.RemoveEmptyEntries);
+            return FromLines(lines);
+        }
+
+        protected void AddAllRows(ICsv csv, string[] lines)
+        {
+            foreach (var line in lines)
+            {
+                AddRow(csv, line);
+            }
+        }
+
+        protected void AddRow(ICsv csv, string line)
+        {
+            var rowData = line.Split(Separator);
+            var rowIndex = csv.NumRows;
+
+            for (var i = 0; i < _numColumns; i++)
+            {
+                csv.SetCell(rowIndex, i, rowData[i]);
+            }
+        }
+
+        protected virtual ICsv GetCsv()
+        {
+            return new Models.Csv
+            {
+                Separator = Separator,
+            };
+        }
+
+        protected virtual void SetHeaders(ICsv csv, string[] lines)
+        {
+            if (!HasHeaders)
+            {
+                return;
+            }
+
+            var headerValues = lines.First()
+                .Split(Separator)
+                .ToArray();
+            var headers = new ICsvHeader[headerValues.Count()];
+
+            for (var i = 0; i < headerValues.Count(); i++)
+            {
+                var newHeader = new Models.CsvHeader(i, headerValues[i]);
+                headers[i] = newHeader;
+            }
+
+            csv.Headers = headers;
+            lines = lines.Skip(1).ToArray();
+        }
+
+        protected virtual void SetHeaders(ICsv csv, StreamReader reader)
+        {
+            if (!HasHeaders)
+            {
+                return;
+            }
+
+            var headerLine = reader.ReadLine();
+            SetHeaders(csv, new string[] { headerLine });
+        }
+    }
+}
